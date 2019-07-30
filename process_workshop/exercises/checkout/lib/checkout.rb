5a6429028e5282ae6f0cdf6c63b463124f197f5f
sejinkay <sejinkay@gmail.com>
2019-07-30 23:31:40 +0100

    now it gives price of item C & D, updated tests

--- process_workshop/exercises/checkout/lib/checkout.rb
+++ process_workshop/exercises/checkout/lib/checkout.rb
@@ -4,6 +4,10 @@ class Shop
       50
     elsif items == "B"
       30
+    elsif items == "C"
+      20
+    elsif items == "D"
+      15
     else
       -1
     end
--- process_workshop/exercises/checkout/spec/checkout_spec.rb
+++ process_workshop/exercises/checkout/spec/checkout_spec.rb
@@ -1,12 +1,22 @@
 require 'checkout'
 
 describe Shop do
-  it 'gives you the price for Item A' do
-    expect(subject.checkout('A')).to eq 50
-  end
+  context 'it returns the price of the single item' do
+    it 'gives you the price for Item A' do
+      expect(subject.checkout('A')).to eq 50
+    end
 
-  it 'gives you the price for Item B' do
-    expect(subject.checkout('B')).to eq 30
+    it 'gives you the price for Item B' do
+      expect(subject.checkout('B')).to eq 30
+    end
+
+    it 'gives you the price for Item C' do
+      expect(subject.checkout('C')).to eq 20
+    end
+
+    it 'gives you the price for Item D' do
+      expect(subject.checkout('D')).to eq 15
+    end
   end
 
   it 'gives you -1 for illegal input' do
